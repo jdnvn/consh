ec3e29b65dfa7b724d772ce8496ec88219c5b8c6
jdnvn <joe@donovan.to>
2024-10-24 22:15:57 -0400

    repl

--- app/main.c
+++ app/main.c
@@ -2,43 +2,48 @@
 #include <stdbool.h>
 #include <string.h>
 
-// void run_command(char command[]) {
-//   print("running %s", command);
-// }
-
-// int strlen(char string[]) {
-//   int i = 0;
-//   while (string[i] != '\0') {
-//     i++;
-//   }
-//   return i;
-// }
+const char* valid_commands[] = {"hi", "exit"};
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
+  while (true) {
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+    fflush(stdout);
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
 
-  int len = strlen(input);
-  if (len > 0 && input[len - 1] == '\n') {
-      input[len - 1] = 0;
-  }
+    int len = strlen(input);
 
-  const char* valid_commands[] = {"hi"};
-  bool is_valid = false;
-
-  for (int i = 0; i < sizeof(valid_commands) / sizeof(valid_commands[0]); i++) {
-    if (strcmp(valid_commands[i], input) == 0) {
-      is_valid = true;
-      break;
+    // just a newline
+    if (len == 1) {
+      continue;
     }
-  }
-  if (!is_valid) {
-    printf("%s: command not found\n", input);
+
+    // strip newline
+    if (len > 0 && input[len - 1] == '\n') {
+        input[len - 1] = 0;
+    }
+
+    bool is_valid = false;
+
+    int i;
+    for (i = 0; i < sizeof(valid_commands) / sizeof(valid_commands[0]); i++) {
+      if (strcmp(valid_commands[i], input) == 0) {
+        is_valid = true;
+        break;
+      }
+    }
+    if (is_valid) {
+      if (i == 0) {
+        printf("HI!!!!\n");
+      } else if (i == 1) {
+        return 0;
+      }
+    } else {
+      printf("%s: command not found\n", input);
+    }
   }
 
   return 0;
